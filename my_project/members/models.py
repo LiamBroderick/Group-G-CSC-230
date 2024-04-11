f6af67f8e2c41aac02f6b90eb9e7889d570c3eb5
FishGoBoom35 <123187143+FishGoBoom35@users.noreply.github.com>
2024-04-10 22:49:34 -0400

    Delete my_project directory

--- my_project/members/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Exhibit(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='exhibit_images/')
