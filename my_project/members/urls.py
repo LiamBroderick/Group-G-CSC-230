62b1c237e8d64a3609de0e15c7e5882640a6ab96
FishGoBoom35 <123187143+FishGoBoom35@users.noreply.github.com>
2024-04-12 14:37:56 -0400

    Add files via upload

---
+++ my_project/members/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('exhibits_page/', views.exhibits_page, name='exhibits_page'),
+    path('science_of_play/', views.science_of_play, name='science_of_play'),
+    path('map/', views.map, name='map'),
+    path('activities/', views.activities, name='activities'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
